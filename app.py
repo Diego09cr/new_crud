81a7cf72e4083500249ae7588031bc40c7b387f6
Diego09cr <diego.criscuolo09@hotmail.com>
2025-08-19 13:20:54 -0300

    feat: create CRUD

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+usuarios = {}
+id = 1
+
+@app.route('/usuario', methods=['POST'])
+def cria():
+    global id
+    dados = request.get_json()
+    usuario = ({'id': id,
+                'nome': dados.get('nome'),
+               'idade': dados.get('idade'),
+               'email': dados.get('email')})
+    usuarios[id] = usuario
+    id += 1
+    return jsonify(usuario)
+
+@app.route('/usuario/<int:id>', methods=['GET'])
+def get_id(id):
+    if id in usuarios:
+        return jsonify(usuarios[id])
+    else:
+        return jsonify({'Usuário': 'Não existe'})
+    
+@app.route('/usuario/<int:id>', methods=['PUT'])
+def att(id):
+    if id in usuarios:
+        dados = request.get_json()
+        usuario = ({'id': id,
+                'nome': dados.get('nome'),
+               'idade': dados.get('idade'),
+               'email': dados.get('email')})
+        usuarios[id] = usuario
+        return jsonify(usuario)
+    else:
+        return jsonify({'Usuário': 'Não existe'})
+
+@app.route('/usuario/<int:id>', methods=['DELETE'])
+def delete(id):
+    if id in usuarios:
+        del(usuarios[id])
+        return jsonify({"Usuário": "deleteado"})
+    else:
+        return jsonify({'Usuário': 'Não existe'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
